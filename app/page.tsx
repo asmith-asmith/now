f4099d35eeb1813ebf3750b8a9a2d33a6b35f0ec
asmith-asmith <aaronmeirsmith@gmail.com>
2025-02-13 12:03:09 -0500

    Changed wording2

--- app/page.tsx
+++ app/page.tsx
@@ -9,18 +9,18 @@ export default function Page() {
 
       <div className="space-y-2">
         <p className="font-medium">Dr. Seuss</p>
-        <p className="text-gray-600">A person's a person, no matter how small</p>
+        <p className="text-gray-600">A person&apos;s a person, no matter how small</p>
       </div>
 
       <div className="space-y-6">
         <h2 className="text-2xl font-medium">About Me</h2>
         <div className="space-y-4 text-gray-600">
           <p>
-            I'm currently a Software Engineer at a large bank. Previously, I worked in the healthcare space in lab
+            I&apos;m currently a Software Engineer at a large bank. Previously, I worked in the healthcare space in lab
             automation as well as drug research.
           </p>
           <p>
-            I'm interested in things. Here are my current favorite{" "}
+            I&apos;m interested in things. Here are my current favorite{" "}
             <Link href="/movies" className="text-gray-900 underline underline-offset-4">
               movies
             </Link>
